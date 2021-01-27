a0541c42dbed93d99e78af860073c977b10bd485
Tushar Agrawal <54900237+tushar432@users.noreply.github.com>
2021-01-27 12:37:52 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "sql301.epizy.com";
-$database = "epiz_27759827_blog";
-$username = "epiz_27759827";
-$password = "DTwcuHgsKylVB";
+$server = "";
+$database = "";
+$username = "";
+$password = "";
 
 $conn = new mysqli($server,$username,$password,$database);
 
